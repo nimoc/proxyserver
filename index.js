5011537683f072240c2b3a07f03ea2f204d67fda
nimojs <nimo.jser@gmail.com>
2015-10-26 12:57:03 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var httpProxy = require('http-proxy')
-var proxy = httpProxy.createProxyServer({})
+var proxyapp = httpProxy.createProxyServer({})
 var express = require('express')
 var app = express()
 /*
@@ -9,17 +9,19 @@ var config = {
     "demo.fms.help": "http://127.0.0.1:3000",
 }
 */
-
-module.exports = function (config, request) {
-    app.use('*', function (req, res) {
+var proxy = function (config, request) {
+    app.use(function (req, res) {
         if (request) {
             request(req,res)
         }
-        var url = config[req.host]
-        if (url) {
-            res.send(req.host + " not in the configuration")
+        var url = config[req.hostname]
+        if (!url) {
+            res.send(req.hostname + " not in the configuration")
         }
-        proxy.web(req, res, { target: url });
+        else {
+            proxyapp.web(req, res, { target: url });
+        }
     })
     app.listen(80)
 }
+module.exports = proxy
