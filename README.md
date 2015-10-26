81fc4751148b6e6f2181499f04110967843d5745
nimojs <nimo.jser@gmail.com>
2015-10-26 12:46:19 +0800

    proxyserver

---
+++ README.md
@@ -0,0 +1,11 @@
+# proxyserver
+
+node 反向代理
+
+
+```js
+var proxy = require("proxyserver")
+proxy({
+    "fms.help": "127.0.0.1:1100"
+})
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+var http = require('http')
+var httpProxy = require('http-proxy')
+var proxy = httpProxy.createProxyServer({})
+var app = express()
+/*
+var config = {
+    "fms.help": "http://127.0.0.1:80",
+    "demo.fms.help": "http://127.0.0.1:3000",
+}
+*/
+
+module.exports = function (config, request) {
+    app.use('*', function (req, res) {
+        if (request) {
+            request(req,res)
+        }
+        var url = config[req.host]
+        if (url) {
+            res.send(req.host + "not in the configuration")
+        }
+        proxy.web(req, res, { target: url });
+    })
+    app.listen(80)
+}
